6d27afcefa61d8f27c80ee2c1d43b35951c64b0d
David29-ra <davidrivasa.12@gmail.com>
2021-12-10 02:24:37 -0500

    Challenge_test to test the examples on readme

--- testing/challenge_test.rb
+++ testing/challenge_test.rb
@@ -1,1 +1,17 @@
 # Script to test class challenge
+require "minitest/autorun"
+require_relative "./io_test"
+require_relative "../challenge"
+require_relative "../modules/balanced"
+
+class ChallengeTest < Minitest::Test
+  include IoTestHelpers
+  include Balanced
+
+  def setup
+    super
+    @challenge = Challenge.new
+  end
+
+  def test_we_can_exit; end
+end
