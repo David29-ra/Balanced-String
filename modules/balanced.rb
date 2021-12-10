85b0cf071c823bc1e9755462cddfab432cd2e72d
David29-ra <davidrivasa.12@gmail.com>
2021-12-10 01:36:11 -0500

    Main functuon to test the messages are done, here I use regex to solve the problem

--- modules/balanced.rb
+++ modules/balanced.rb
@@ -1,1 +1,18 @@
 # Module with the function to check if a string is balanced
+
+module Balanced
+  def its_balanced?(string)
+    string = string.gsub(/(:\(|:\)|[a-z]| |:|\([a-z]*:[a-z]*\))/, "")
+    valid_braces(string) ? "Balanceado" : "Desbalanceado"
+  end
+
+  def valid_braces(braces)
+    stack = []
+    symbols = { "(" => ")" }
+
+    braces.each_char do |brace|
+      stack.last && stack.last == symbols.key(brace) ? stack.pop : stack << brace
+    end
+    stack.empty?
+  end
+end
