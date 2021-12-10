017e9599a5e2d5502fa2ba9a23e91659f706dcb3
David29-ra <davidrivasa.12@gmail.com>
2021-12-10 01:57:27 -0500

    method start are done, now its time to implement function bbalanced or not

--- challenge.rb
+++ challenge.rb
@@ -1,8 +1,28 @@
 # Class Challenge
+require_relative "./modules/presenter"
+require_relative "./modules/requester"
+require_relative "./modules/balanced"
+
 class Challenge
+  include Presenter
+  include Requester
+  include Balanced
+
   def initialize; end
 
   def start
-    puts "hola"
+    puts say_welcome
+    action = ""
+    until action == "exit"
+      action = select_action
+      case action
+      when "test" then balanced_or_not
+      end
+    end
+    puts say_goodbye
+  end
+
+  def balanced_or_not
+    puts "here"
   end
 end
